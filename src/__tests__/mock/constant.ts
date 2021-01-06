f4f550f87d74f8d13d8e10aac384be701f90a476
Bhushankumar L <bhushankumar.lilapara@gmail.com>
2021-01-06 14:58:29 +0530

    add basic endpoints and test case

---
+++ src/__tests__/mock/constant.ts
@@ -0,0 +1,7 @@
+export const MOCK = {
+    SAMPLE_REQUEST_URL_1: 'https://mylocation.org'
+}
+
+export const ENVs = {
+    ACCESS_TOKEN: process.env.QS_ACCESS_TOKEN
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,35 @@
+import got from 'got/dist/source';
+import { APP } from './constants';
+
 export class QuickScraper {
-    public getHtml(name: string): string {
-        return `Hello ${name}`;
+    private parseUrl: string;
+    private accessToken = '';
+
+    constructor(accessToken?: string) {
+        this.parseUrl = APP.BASE_URL.concat('parse');
+        if (accessToken) {
+            this.setAccessToken(accessToken);
+        }
+    }
+
+    public setAccessToken(accessToken: string) {
+        this.accessToken = accessToken;
+    }
+
+    private prepareRequestUrl(url: string): string {
+        const requestUrl = this.parseUrl.concat
+            ('?', 'access_token=', this.accessToken,
+                '&', 'URL=', url);
+        // console.log('requestUrl ', requestUrl);
+        return requestUrl;
+    }
+
+    public async getHtml(url: string): Promise<string> {
+        const requestUrl = this.prepareRequestUrl(url);
+        console.log('requestUrl ', requestUrl);
+        const response = await got(requestUrl);
+        console.log('response');
+        console.log(response.body);
+        return response.body;
     }
 }
