11664629abb917ccf6a804ed0852500cc5054e49
Bhushankumar L <bhushankumar.lilapara@gmail.com>
2021-01-12 14:25:21 +0530

    added debug package and remove console

--- src/__tests__/Parse.test.ts
+++ src/__tests__/Parse.test.ts
@@ -1,18 +1,19 @@
-/* eslint-disable no-console */
+import Debug from 'debug';
 import { QuickScraper } from '../index';
 import { ENVs, MOCK } from './mock/constant';
 const QuickScraperClient = new QuickScraper();
+const debug = Debug('QS:Parse.test.ts');
 
 test('Parse URL', async () => {
   const requestUrl = MOCK.SAMPLE_REQUEST_URL_1;
-  console.log('ENVs.ACCESS_TOKEN ', ENVs.ACCESS_TOKEN);
+  debug('ENVs.ACCESS_TOKEN ', ENVs.ACCESS_TOKEN);
   QuickScraperClient.setAccessToken(ENVs.ACCESS_TOKEN)
   try {
     const response = await QuickScraperClient.getHtml(requestUrl);
     expect(response).not.toBeNull();
     expect(response).not.toBeUndefined();
   } catch (error) {
-    console.log('error ', error);
+    debug('error ', error);
     expect(error).toBeNull();
   }
 });
