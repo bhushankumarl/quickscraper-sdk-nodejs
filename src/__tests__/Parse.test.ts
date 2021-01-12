56f97c81730c30134ecabc18e5c6b0758b117217
Bhushankumar L <bhushankumar.lilapara@gmail.com>
2021-01-12 14:30:03 +0530

    updated the ENVs

--- src/__tests__/Parse.test.ts
+++ src/__tests__/Parse.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import Debug from 'debug';
 import { QuickScraper } from '../index';
 import { ENVs, MOCK } from './mock/constant';
@@ -7,6 +8,7 @@ const debug = Debug('QS:Parse.test.ts');
 test('Parse URL', async () => {
   const requestUrl = MOCK.SAMPLE_REQUEST_URL_1;
   debug('ENVs.ACCESS_TOKEN ', ENVs.ACCESS_TOKEN);
+  console.log('ENVs.ACCESS_TOKEN ', ENVs.ACCESS_TOKEN);
   QuickScraperClient.setAccessToken(ENVs.ACCESS_TOKEN)
   try {
     const response = await QuickScraperClient.getHtml(requestUrl);
--- src/__tests__/mock/constant.ts
+++ src/__tests__/mock/constant.ts
@@ -1,3 +1,7 @@
+/* eslint-disable no-console */
+import Debug from 'debug';
+const debug = Debug('QS:Constant.ts');
+
 export const MOCK = {
   SAMPLE_REQUEST_URL_1: 'https://mylocation.org'
 }
@@ -6,8 +10,9 @@ export const ENVs = {
   ACCESS_TOKEN: process.env.QS_ACCESS_TOKEN || ''
 }
 
-// console.log('ENVs ', ENVs);
-// console.log('MOCK ', MOCK);
+console.log('ENVs ', ENVs);
+console.log('MOCK ', MOCK);
+debug('MOCK ', MOCK);
 
 test('Validate ENVs value should be set correct.', async () => {
   expect(MOCK.SAMPLE_REQUEST_URL_1).not.toBeUndefined();
