9444b62f123f27161d6b122dc503899094f32c3d
Bhushankumar L <bhushankumar.lilapara@gmail.com>
2021-01-12 14:19:33 +0530

    allow console

--- src/__tests__/Parse.test.ts
+++ src/__tests__/Parse.test.ts
@@ -1,16 +1,18 @@
+/* eslint-disable no-console */
 import { QuickScraper } from '../index';
 import { ENVs, MOCK } from './mock/constant';
 const QuickScraperClient = new QuickScraper();
 
 test('Parse URL', async () => {
   const requestUrl = MOCK.SAMPLE_REQUEST_URL_1;
+  console.log('ENVs.ACCESS_TOKEN ', ENVs.ACCESS_TOKEN);
   QuickScraperClient.setAccessToken(ENVs.ACCESS_TOKEN)
   try {
     const response = await QuickScraperClient.getHtml(requestUrl);
     expect(response).not.toBeNull();
     expect(response).not.toBeUndefined();
   } catch (error) {
-    // console.log('error ', error);
+    console.log('error ', error);
     expect(error).toBeNull();
   }
 });
